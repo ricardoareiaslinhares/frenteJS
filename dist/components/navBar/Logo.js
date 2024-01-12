42cc05f0c31cecb6ddc8f5a815e6c23def3ccc55
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-12 00:13:06 +0000

    logo img bug

--- dist/components/navBar/Logo.js
+++ dist/components/navBar/Logo.js
@@ -4,6 +4,8 @@ const _Logo = (parentElement) => {
     const logo = Frente.createElement(`
     <div id="${idLogo}" class="w-[40px] h-auto flex items-center justify-center ">
     <img src="../../../assets/logo.png" width="40" height="40" alt="Ricardo Linhares Logo" >
+    <img src="/assets/logo.png" alt="Ricardo Linhares Logo" >
+    <img src="assets/logo.png" width="40" height="40" alt="Ricardo Linhares Logo" >
     </div>
     `, { parentElement });
     return logo;
