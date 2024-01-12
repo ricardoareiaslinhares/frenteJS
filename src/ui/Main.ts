304410d05d56afbbe9b2295170967199010a06db
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-12 00:43:49 +0000

    scroll bug on mobile

--- src/ui/Main.ts
+++ src/ui/Main.ts
@@ -8,7 +8,7 @@ const idMain = Frente.generateId()
 
 
     const _main = Frente.createElement(`
-        <main id="${idMain}" class="max-w-screen-2xl flex max-h-screen flex-col gap-3 overflow-auto 2xl:justify-center 2xl:items-center">
+        <main id="${idMain}" class="max-w-screen-2xl flex max-h-screen flex-col gap-3 overflow-scroll lg:overflow-auto 2xl:justify-center 2xl:items-center">
         </main>
     `,{parentElement})
 
