447cb4cb5090c10062ce14540435c2dbcba52c4b
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-25 17:32:56 +0000

    improved site bigscreen resp

--- dist/ui/Main.js
+++ dist/ui/Main.js
@@ -2,7 +2,7 @@ import Frente from "../FrenteJS/frente.js";
 const _Main = (parentElement) => {
     const idMain = Frente.generateId();
     const _main = Frente.createElement(`
-        <main id="${idMain}" class="max-w-screen-2xl flex max-h-screen flex-col gap-3 2xl:justify-center 2xl:items-center">
+        <main id="${idMain}" class=" flex max-h-screen flex-col gap-3 2xl:justify-center 2xl:items-center">
         </main>
     `, { parentElement });
     return { _main: _main.element, ids: { idMain } };
--- src/ui/Section.ts
+++ src/ui/Section.ts
@@ -9,7 +9,7 @@ const idSection = Frente.generateId()
 
 
     const section = Frente.createElement(`
-        <section id="${idSection}" class="max-w-screen-2xl w-full flex flex-col justify-center items-center p-12 gap-3"></section>
+        <section id="${idSection}" class=" w-full flex flex-col  justify-center items-center p-12 gap-3"></section>
     `,{parentElement })
   return {_Section:section.element, ids:{idSection}}
 }
